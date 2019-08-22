fbafde47dedeb2b6880f143cfaa5e28f1682eaab
jssantosh84 <santoshjogur@192.168.29.136>
2019-08-22 21:34:40 +0530

    sample commit

--- iOSLearningApp/ViewController.m
+++ iOSLearningApp/ViewController.m
@@ -43,6 +43,7 @@
 }
 
 - (void)viewDidLoad {
+
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     whirl = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
